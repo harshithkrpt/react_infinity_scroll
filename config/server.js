b2dbc99538618536f4b9d14a5f6f1de3684bbb04
Harshith <harshith.krpt@gmail.com>
2019-02-22 18:20:55 +0530

    Final Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = {
+  APPLICATION_ID:
+    process.env.APPLICATION_ID ||
+    "77651181abd9312ec95fa0af481851767a4dd1a54ae5358caa6010395b4513cf",
+  SECRET:
+    process.env.SECRET ||
+    "e7508f9c486cbf8fbc04a896e6d2ba5fd9cb0715d4a7617f4b8454af84af407f",
+  CALLBACK_URL: process.env.CALLBACK_URL || "http://localhost:3000"
+};
